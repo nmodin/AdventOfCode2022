befa3c92cc8b36a57610324f55e639cdbc0c7cb6
Niklas Modin <niklas.modin-konsult@pliktverket.se>
2022-12-05 08:30:54 +0100

    Added day 2

---
+++ src/main/java/org/example/Day2.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class Day2 extends AdventOfCode2022 {
+
+    @Override
+    public int getDayNumber() {
+        return 1;
+    }
+
+    @Override
+    final protected void solvePart1(List<String> input) {
+        System.out.println("Day1.runPart1: " + "N/A");
+    }
+
+    @Override
+    public void solvePart2(List<String> input) {
+        System.out.println("Day1.runPart2: " + "N/A");
+    }
+}
